e23d4b7ca703e8a6bb736ba70b5a6d9586a82e71
yechanYang <yac392045@mokpo.ac.kr>
2023-06-20 20:30:06 +0900

    first commit

---
+++ Http_study/src/main/java/com/example/http_study/HttpStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.http_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpStudyApplication.class, args);
+    }
+
+}
